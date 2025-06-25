894cfeb57e3b9fb1c3556d1619d0220cf63e1d0f
youssup <youssupsong@gmail.com>
2025-06-24 20:48:54 -0400

    feat: commits

--- git.js
+++ git.js
@@ -1,5 +1,13 @@
 var repo = new Git("newRepo")
 
+// Constructor for Git object
 function Git(name) {
     this.name = name;
 }
+
+// Method to add a file to the repository
+function Commit(id, message) {
+    this.id = id;
+    this.message = message;
+}
+
